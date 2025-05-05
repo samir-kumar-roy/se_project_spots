7ecfdb0645ca0102ec3cb8dcc4b6eb0c396cf2d3
Samir Roy <samirroy594@gmail.com>
2025-05-05 18:01:17 -0500

    refactor code in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,3 @@
-const initialCards = [
-  {
-    name: "Magnolia Springs",
-    link: "https://www.travelandleisure.com/thmb/HskLNeR7eVEZerVuOD4bIq0tW0g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/magnolia-springs-alabama-BEAUTYSTS0522-dd0254bbad464ecab9ebbe64b45e32f2.jpg",
-  },
-  {
-    name: "Elisa National Park",
-    link: "https://www.travelandleisure.com/thmb/g727-mPNmnsphYdSQ2_7PW74KvQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/wrangell-st-elias-national-park-alaska-BEAUTYSTS0522-aa733bbb6af64cd4b64d2672a30bc8ac.jpg",
-  },
-  {
-    name: "Grand Canyon National Park",
-    link: "https://www.travelandleisure.com/thmb/Ci2_pM0Qf9mBBTw9ij3Oindooqc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GrandCanyon_DonEim02-1af7fa1cf4704ef585e7a700b2aa7da5.jpg",
-  },
-  {
-    name: "St Francis National Park",
-    link: "https://www.travelandleisure.com/thmb/3LElCC6HD02cJF7KdAN_Z52pwpI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/ozark-national-forest-arkansas-BEAUTYSTS0522-c7606af79d004d6f92332d73729f42ca.jpg",
-  },
-  {
-    name: "Yosemite National Park",
-    link: "https://www.travelandleisure.com/thmb/BdEJ459gqdh0nBpauUuTZO46GCQ=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Yosemite_VinceFergus02-37fd80faef6546eab4e1d9c681283162.jpg",
-  },
-  {
-    name: "Gardens of the Gods",
-    link: "https://www.travelandleisure.com/thmb/B1XWpAn2EERgkZDTtiqVt_ofbgc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/garden-of-the-gods-colorado-BEAUTYSTS0522-17d3f1da19434e11a1def9559e8e6997.jpg",
-  },
-];
 const editProfileButton = document.querySelector(".profile__edit-button");
 const newPostButton = document.querySelector(".profile__post-button");
 
@@ -37,26 +11,26 @@ const newPostModalCloseButton = newPostModal.querySelector(".modal__close-btn");
 const previewModalCloseButton = previewModal.querySelector(
   ".modal__close-btn_type_preview"
 );
-const profileFormElement = document.forms["profile-form"];
+const editProfileFormElement = document.forms["profile-form"];
+const editProfileElements = editProfileFormElement.elements;
 const newPostFormElement = document.forms["new-post-form"];
 // const editFormElement = editModal.querySelector(".modal__form");
-const editFormNameElement = document.querySelector("#name");
-const editFormDescriptionElement = document.querySelector("#description");
+const editFormNameElement = editProfileElements[0];
+const editFormDescriptionElement = editProfileElements[1];
 const profileNameElement = document.querySelector(".profile__name");
 const profileDescriptionElement = document.querySelector(
   ".profile__description"
 );
+const newPostElements = newPostFormElement.elements;
 const cardsList = document.querySelector(".cards__list");
 
 editProfileButton.addEventListener("click", function (e) {
-  e.preventDefault();
   editFormNameElement.value = profileNameElement.textContent;
   editFormDescriptionElement.value = profileDescriptionElement.textContent;
   openModal(editModal);
 });
 // new post modal opening
 newPostButton.addEventListener("click", function (e) {
-  e.preventDefault();
   openModal(newPostModal);
 });
 // form submit handlerFunction
@@ -69,17 +43,16 @@ function editProfileSubmitHandler(e) {
 function newPostSubmitHandler(e) {
   e.preventDefault();
   // something to do with form element
-  const imageLink = document.querySelector("#image-link").value;
-  const imageCaption = document.querySelector("#image-caption").value;
+  const imageLink = newPostElements[0].value;
+  const imageCaption = newPostElements[1].value;
   const newCardData = { name: imageCaption, link: imageLink };
   initialCards.unshift(newCardData);
   const cardEl = getCardElement(newCardData);
-  console.log(cardEl);
   cardsList.prepend(cardEl);
   closeModal(newPostModal);
 }
 // Edit form submit button handling
-profileFormElement.addEventListener("submit", editProfileSubmitHandler);
+editProfileFormElement.addEventListener("submit", editProfileSubmitHandler);
 newPostFormElement.addEventListener("submit", newPostSubmitHandler);
 editModalCloseButton.addEventListener("click", function () {
   closeModal(editModal);
@@ -119,11 +92,14 @@ function getCardElement(data) {
 
   return cardElement;
 }
+// function for adding card to a section using either prepend, append or any other method
+function renderCard(item, method = "prepend") {
+  const cardElement = getCardElement(item);
+  cardsList[method](cardElement);
+}
 // Looping through initialCards array items using forEach loop
 initialCards.forEach(function (card) {
-  // console.log(card.name);
-  const cardEl = getCardElement(card);
-  cardsList.append(cardEl);
+  renderCard(card, "prepend");
 });
 // opening and closing of modal
 function openModal(modal) {
